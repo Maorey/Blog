6e2241f6513bb372ce14088af200d35771a85609
毛瑞 <Maorey@Foxmail.com>
2021-06-04 19:39:00 +0800

    blog: 凸凸凸

--- docs/blog/algorithm/components/melkman/divide.ts
+++ docs/blog/algorithm/components/melkman/divide.ts
@@ -1,40 +1,5 @@
 import type { Point, Algorithm } from './types'
 
-const insert = (point: Point, points: Point[], sortField: keyof Point) => {
-  // 二分查找 [low, high)
-  const value = point[sortField]
-  let low = 0
-  let high = points.length
-  let i: number
-  while (low < high) {
-    i = (low + high) >> 1 // 除2并向下取整
-    if (points[i][sortField] > value) {
-      high = i - 1
-    } else {
-      low = i + 1
-    }
-  }
-
-  // 插入
-  // points.splice(low, 0, point)
-  i = points.length
-  while (i > low) {
-    points[i] = points[--i]
-  }
-  points[low] = point
-}
-
-/** 划分角落
- * 上下极值点y坐标相等时, 按x坐标升序
- * 左右极值点x坐标相等时, 按y坐标升序
- */
-const enum Corner {
-  all,
-  leftTop,
-  rightTop,
-  leftBottom,
-  rightBottom,
-}
 /** 比较极值点
  * @param extreme 当前极值点
  * @param point
@@ -42,128 +7,141 @@ const enum Corner {
  * @returns false: 非极值点 true: 新极值点; x/y(排序字段): 与当前极值点相等
  */
 type Compare = (extreme: Point, point: Point) => boolean | keyof Point
-/** 更新极值点集
- * @param point
- * @param extremes
- * @param compare
- */
-const updateExtremes = (point: Point, extremes: Point[], compare: Compare) => {
-  if (extremes.length) {
-    const result = compare(extremes[0], point)
-    if (result === true) {
-      extremes.splice(0, extremes.length, point)
-    } else if (result) {
-      insert(point, extremes, result)
-    }
-  } else {
-    extremes.push(point)
-  }
-}
 const compareLeft: Compare = (e, p) => (e.x === p.x ? 'y' : e.x > p.x)
 const compareRight: Compare = (e, p) => (e.x === p.x ? 'y' : e.x < p.x)
 const compareTop: Compare = (e, p) => (e.y === p.y ? 'x' : e.y < p.y)
 const compareBottom: Compare = (e, p) => (e.y === p.y ? 'x' : e.y > p.y)
 
-interface VectorInfo {
-  x: number
-  y: number
-  dx: number
-  dy: number
-}
-const getVectorInfo = (
-  xPoints: Point[],
-  yPoints: Point[],
-  corner: Exclude<Corner, Corner.all>
-): VectorInfo => {
-  let p0: Point
-  let p1: Point
-  switch (corner) {
-    case Corner.leftTop:
-      p0 = yPoints[0]
-      p1 = xPoints[xPoints.length - 1]
-      break
-    case Corner.rightTop:
-      p0 = xPoints[yPoints.length - 1]
-      p1 = yPoints[xPoints.length - 1]
-      break
-    case Corner.leftBottom:
-      p0 = xPoints[0]
-      p1 = yPoints[0]
-      break
-    case Corner.rightBottom:
-      p0 = yPoints[yPoints.length - 1]
-      p1 = xPoints[0]
-      break
+/** 更新极值点集
+ * @param point
+ * @param extremes 【最多保留两个】
+ * @param compare
+ *
+ * @returns 是否更新了极值点
+ */
+const updateExtremes = (point: Point, extremes: Point[], compare: Compare) => {
+  if (!extremes.length) {
+    extremes.push(point)
+    return true
   }
 
-  const { x, y } = p0
-  return { x, y, dx: p1.x - x, dy: p1.y - y }
-}
-const isClockwise = (point: Point, vectorInfo: VectorInfo) =>
-  vectorInfo.dx * (point.y - vectorInfo.y) > (point.x - vectorInfo.x) * vectorInfo.dy
+  const extreme0 = extremes[0]
+  const result = compare(extreme0, point)
 
-interface UncertainWithCorner extends Array<Point> {
-  corner: Exclude<Corner, Corner.all>
+  if (!result) {
+    return
+  }
+
+  if (result === true) {
+    extremes.splice(0, 2, point)
+    return true
+  }
+
+  const value = point[result]
+  const min = extreme0[result]
+  const extreme1 = extremes[1]
+  if (value > min && (!extreme1 || value > extreme1[result])) {
+    extremes[1] = point
+
+    return true
+  }
+  if (value < min) {
+    extremes[0] = point
+    extreme1 || (extremes[1] = extreme0)
+
+    return true
+  }
+}
+
+type InRange = (point: Point, xExtremes: Point[], yExtremes: Point[]) => boolean
+const inLeftTopRange: InRange = (point, leftExtremes, topExtremes) =>
+  point.x < topExtremes[0].x && point.y > leftExtremes[leftExtremes.length - 1].y
+const inRightTopRange: InRange = (point, rightExtremes, topExtremes) =>
+  point.x > topExtremes[topExtremes.length - 1].x &&
+  point.y > rightExtremes[rightExtremes.length - 1].y
+const inLeftBottomRange: InRange = (point, leftExtremes, bottomExtremes) =>
+  point.x < bottomExtremes[0].x && point.y < leftExtremes[0].y
+const inRightBottomRange: InRange = (point, rightExtremes, bottomExtremes) =>
+  point.x > bottomExtremes[bottomExtremes.length - 1].x && point.y < rightExtremes[0].y
+
+type GetVector = (xExtremes: Point[], yExtremes: Point[]) => [Point, Point]
+const getLeftTopVector: GetVector = (leftExtremes, topExtremes) => [
+  topExtremes[0],
+  leftExtremes[leftExtremes.length - 1],
+]
+const getRightTopVector: GetVector = (rightExtremes, topExtremes) => [
+  rightExtremes[rightExtremes.length - 1],
+  topExtremes[topExtremes.length - 1],
+]
+const getLeftBottomVector: GetVector = (leftExtremes, bottomExtremes) => [
+  leftExtremes[0],
+  bottomExtremes[0],
+]
+const getRightBottomVector: GetVector = (rightExtremes, bottomExtremes) => [
+  bottomExtremes[bottomExtremes.length - 1],
+  rightExtremes[0],
+]
+
+const isClockwise = (point: Point, vector: [Point, Point]) =>
+  (vector[1].x - vector[0].x) * (point.y - vector[0].y) >
+  (point.x - vector[0].x) * (vector[1].y - vector[0].y)
+
+/** 划分角落
+ * 上下极值点y坐标相等时, 按x坐标升序
+ * 左右极值点x坐标相等时, 按y坐标升序
+ */
+const enum Corner {
+  leftTop,
+  rightTop,
+  leftBottom,
+  rightBottom,
+}
+
+interface CornerPoints extends Array<Point> {
+  c: Corner
+  x: Compare
+  y: Compare
+  i: InRange
+  v: [Point, Point]
 }
 /** 对指定的四个角落之一进行划分
- * @param points
+ * @param cornerPoints
  * @param corner
  * @returns [x极值点集, y极值点集, 相同角落待确定点集]
  */
-const partitionCorner = (
-  points: Point[],
-  corner: Exclude<Corner, Corner.all>
-): [Point[], Point[], UncertainWithCorner] => {
+const partitionCorner = (cornerPoints: CornerPoints): [Point[], Point[], CornerPoints] => {
   const xPoints: Point[] = []
   const yPoints: Point[] = []
 
-  let i = points.length
+  let i = cornerPoints.length
   if (i < 2) {
-    return [points, xPoints, yPoints as UncertainWithCorner]
-  }
-
-  let compareX: Compare
-  let compareY: Compare
-  switch (corner) {
-    case Corner.leftTop:
-      compareX = compareLeft
-      compareY = compareTop
-      break
-    case Corner.rightTop:
-      compareX = compareRight
-      compareY = compareTop
-      break
-    case Corner.leftBottom:
-      compareX = compareLeft
-      compareY = compareBottom
-      break
-    case Corner.rightBottom:
-      compareX = compareRight
-      compareY = compareBottom
-      break
-    default:
-      console.error('partitionCorner: unknown corner ' + corner)
-      return [xPoints, yPoints, yPoints as UncertainWithCorner]
+    return [cornerPoints, xPoints, yPoints as CornerPoints]
   }
 
   let point: Point
   while (i) {
-    point = points[--i]
+    point = cornerPoints[--i]
 
-    updateExtremes(point, xPoints, compareX)
-    updateExtremes(point, yPoints, compareY)
+    updateExtremes(point, xPoints, cornerPoints.x)
+    updateExtremes(point, yPoints, cornerPoints.y)
   }
 
-  const uncertain: UncertainWithCorner = [] as any
-  uncertain.corner = corner
-  const vectorInfo = getVectorInfo(xPoints, yPoints, corner)
-  i = points.length
+  const restCornerPoints: CornerPoints = [] as any
+  restCornerPoints.c = cornerPoints.c
+  restCornerPoints.x = cornerPoints.x
+  restCornerPoints.y = cornerPoints.y
+  restCornerPoints.i = cornerPoints.i
+  restCornerPoints.v = cornerPoints.v
+
+  i = cornerPoints.length
   while (i) {
-    point = points[--i]
-    isClockwise(point, vectorInfo) && uncertain.push(point)
+    point = cornerPoints[--i]
+    cornerPoints.i(point, xPoints, yPoints) &&
+      isClockwise(point, cornerPoints.v) &&
+      restCornerPoints.push(point)
   }
 
-  return [xPoints, yPoints, uncertain]
+  return [xPoints, yPoints, restCornerPoints]
 }
 
 /** 将点集划分为5个区域
@@ -172,64 +150,77 @@ const partitionCorner = (
  */
 const partition = (
   points: Point[]
-):
-  | void
-  | [
-      [Point[], Point[], Point[], Point[]],
-      [UncertainWithCorner, UncertainWithCorner, UncertainWithCorner, UncertainWithCorner]
-    ] => {
-  const leftPoints: Point[] = []
-  const rightPoints: Point[] = []
-  const topPoints: Point[] = []
-  const bottomPoints: Point[] = []
-
+): void | [[Point[], Point[], Point[], Point[]], CornerPoints[]] => {
   let i = points.length
   if (i < 3) {
     return
   }
 
+  const leftExtremes: Point[] = []
+  const rightExtremes: Point[] = []
+  const topExtremes: Point[] = []
+  const bottomExtremes: Point[] = []
+
   let point: Point
   while (i) {
     point = points[--i]
 
-    updateExtremes(point, leftPoints, compareLeft)
-    updateExtremes(point, rightPoints, compareRight)
-    updateExtremes(point, topPoints, compareTop)
-    updateExtremes(point, bottomPoints, compareBottom)
+    updateExtremes(point, leftExtremes, compareLeft)
+    updateExtremes(point, rightExtremes, compareRight)
+    updateExtremes(point, topExtremes, compareTop)
+    updateExtremes(point, bottomExtremes, compareBottom)
   }
 
-  const leftTopUncertain: UncertainWithCorner = [] as any
-  const leftTopVectorInfo = getVectorInfo(leftPoints, topPoints, Corner.leftTop)
-  leftTopUncertain.corner = Corner.leftTop
+  const leftTopCornerPoints: CornerPoints = [] as any
+  const leftTopVector = getLeftTopVector(leftExtremes, topExtremes)
+  leftTopCornerPoints.c = Corner.leftTop
+  leftTopCornerPoints.x = compareLeft
+  leftTopCornerPoints.y = compareTop
+  leftTopCornerPoints.i = inLeftTopRange
+  leftTopCornerPoints.v = leftTopVector
 
-  const rightTopUncertain: UncertainWithCorner = [] as any
-  const rightTopVectorInfo = getVectorInfo(rightPoints, topPoints, Corner.rightTop)
-  rightTopUncertain.corner = Corner.rightTop
+  const rightTopCornerPoints: CornerPoints = [] as any
+  const rightTopVector = getRightTopVector(rightExtremes, topExtremes)
+  rightTopCornerPoints.c = Corner.rightTop
+  rightTopCornerPoints.x = compareRight
+  rightTopCornerPoints.y = compareTop
+  rightTopCornerPoints.i = inRightTopRange
+  rightTopCornerPoints.v = rightTopVector
 
-  const leftBottomUncertain: UncertainWithCorner = [] as any
-  const leftBottomVectorInfo = getVectorInfo(leftPoints, bottomPoints, Corner.leftBottom)
-  leftBottomUncertain.corner = Corner.leftBottom
+  const leftBottomCornerPoints: CornerPoints = [] as any
+  const leftBottomVector = getLeftBottomVector(leftExtremes, bottomExtremes)
+  leftBottomCornerPoints.c = Corner.leftBottom
+  leftBottomCornerPoints.x = compareLeft
+  leftBottomCornerPoints.y = compareBottom
+  leftBottomCornerPoints.i = inLeftBottomRange
+  leftBottomCornerPoints.v = leftBottomVector
 
-  const rightBottomUncertain: UncertainWithCorner = [] as any
-  const rightBottomVectorInfo = getVectorInfo(rightPoints, bottomPoints, Corner.rightBottom)
-  rightBottomUncertain.corner = Corner.rightBottom
+  const rightBottomCornerPoints: CornerPoints = [] as any
+  const rightBottomVector = getRightBottomVector(rightExtremes, bottomExtremes)
+  rightBottomCornerPoints.c = Corner.rightBottom
+  rightBottomCornerPoints.x = compareRight
+  rightBottomCornerPoints.y = compareBottom
+  rightBottomCornerPoints.i = inRightBottomRange
+  rightBottomCornerPoints.v = rightBottomVector
 
   i = points.length
   while (i) {
     point = points[--i]
 
-    isClockwise(point, leftTopVectorInfo)
-      ? leftTopUncertain.push(point)
-      : isClockwise(point, rightTopVectorInfo)
-      ? rightTopUncertain.push(point)
-      : isClockwise(point, leftBottomVectorInfo)
-      ? leftBottomUncertain.push(point)
-      : isClockwise(point, rightBottomVectorInfo) && rightBottomUncertain.push(point)
+    inLeftTopRange(point, leftExtremes, topExtremes)
+      ? isClockwise(point, leftTopVector) && leftTopCornerPoints.push(point)
+      : inRightTopRange(point, rightExtremes, topExtremes)
+      ? isClockwise(point, rightTopVector) && rightTopCornerPoints.push(point)
+      : inLeftBottomRange(point, leftExtremes, bottomExtremes)
+      ? isClockwise(point, leftBottomVector) && leftBottomCornerPoints.push(point)
+      : inRightBottomRange(point, rightExtremes, bottomExtremes) &&
+        isClockwise(point, rightBottomVector) &&
+        rightBottomCornerPoints.push(point)
   }
 
   return [
-    [leftPoints, rightPoints, topPoints, bottomPoints],
-    [leftTopUncertain, rightTopUncertain, leftBottomUncertain, rightBottomUncertain],
+    [leftExtremes, rightExtremes, topExtremes, bottomExtremes],
+    [leftTopCornerPoints, rightTopCornerPoints, leftBottomCornerPoints, rightBottomCornerPoints],
   ]
 }
 
@@ -265,41 +256,40 @@ const divide: Algorithm = function* (points) {
     return points
   }
 
-  const [extremes, uncertain] = partitionResult
-  const [leftPoints, rightPoints, topPoints, bottomPoints] = extremes
+  const [[leftPoints, rightPoints, topPoints, bottomPoints], cornerPointsList] = partitionResult
 
-  let uncertainPoints: UncertainWithCorner
-  let xPoints: Point[]
-  let yPoints: Point[]
-  while (uncertain.length) {
-    yield generateConvex.apply(null, extremes)
+  let cornerPoints: CornerPoints
+  let xExtremes: Point[]
+  let yExtremes: Point[]
+  while (cornerPointsList.length) {
+    yield generateConvex(leftPoints, rightPoints, topPoints, bottomPoints)
 
-    uncertainPoints = uncertain.shift()!
-    ;[xPoints, yPoints, uncertainPoints] = partitionCorner(uncertainPoints, uncertainPoints.corner)
+    cornerPoints = cornerPointsList.shift()!
+    ;[xExtremes, yExtremes, cornerPoints] = partitionCorner(cornerPoints)
 
-    switch (uncertainPoints.corner) {
+    switch (cornerPoints.c) {
       case Corner.leftTop:
-        leftPoints.push(...xPoints)
-        topPoints.unshift(...yPoints)
+        leftPoints.push(...xExtremes)
+        topPoints.unshift(...yExtremes)
         break
       case Corner.rightTop:
-        rightPoints.push(...xPoints)
-        topPoints.push(...yPoints)
+        rightPoints.push(...xExtremes)
+        topPoints.push(...yExtremes)
         break
       case Corner.leftBottom:
-        leftPoints.unshift(...xPoints)
-        bottomPoints.unshift(...yPoints)
+        leftPoints.unshift(...xExtremes)
+        bottomPoints.unshift(...yExtremes)
         break
       case Corner.rightBottom:
-        rightPoints.unshift(...xPoints)
-        bottomPoints.push(...yPoints)
+        rightPoints.unshift(...xExtremes)
+        bottomPoints.push(...yExtremes)
         break
     }
 
-    uncertainPoints.length && uncertain.push(uncertainPoints)
+    cornerPoints.length && cornerPointsList.push(cornerPoints)
   }
 
-  return generateConvex.apply(null, extremes)
+  return generateConvex(leftPoints, rightPoints, topPoints, bottomPoints)
 }
 
 export default divide
